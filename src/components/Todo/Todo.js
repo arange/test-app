202f9313dd7ffb1c4077c4a8f6c81866a281114a
Pan Zhan <pan.zhan@educationhorizons.com>
2023-08-11 21:40:09 +1000

    feat(init): :sparkles: task init

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Todo = () => {
+  /*
+  You need to: 
+  1. create the necessary React components: one for the overall application, one for displaying the list of to-dos, and one for adding new to-dos.
+  2. set up state management for the to-do items. This could involve using React's useState hook to manage the list of to-dos.
+  3. map through the list of to-dos and render them on the screen
+  4. implement a feature that allows users to add new to-dos
+  Optional:
+  1. apply basic CSS to make the application look presentable
+  2. write a simple test case (e.g. add a new todo and it should be displayed on the screen)
+  */
+  return <div></div>;
+};
